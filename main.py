664d3607e43c5c281be65afe8c40827e50b261d5
Kermitofx <39018448+Kermitofx@users.noreply.github.com>
2019-11-11 01:25:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -182,8 +182,7 @@ def main():
     # Create the Updater and pass it your bot's token.
 
 
-    updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
-    updater.bot.set_webhook("https://filmaffinitybot.herokuapp.com/" + TOKEN)
+   
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
     filmaffinity = FilmaffinityBot()
